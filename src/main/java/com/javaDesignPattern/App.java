55a798f5c1e161d63611d32dd0e35dd63082d550
hechangfeng <515018620@qq.com>
2020-08-30 22:07:21 +0800

    2020.8.30: 1.create project and commit to github test

---
+++ src/main/java/com/javaDesignPattern/App.java
@@ -0,0 +1,13 @@
+package com.javaDesignPattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
